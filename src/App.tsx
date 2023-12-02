a8245aaebb028bac1d7cd48352d116c0ecd9fa5c
iamrishavj <jrishav101@gmail.com>
2023-12-02 14:52:50 +0530

    Comments clean up

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ function App() {
   return (
     <div class="flex flex-col h-screen overflow-hidden bg-gradient-to-r from-blue-500 to-purple-600">
       <div class="flex-none" style="height: 33.333%;">
-        {/* Timer or other content here */}
-        {/* Timer Component */}
         <PomodoroTimer />
       </div>
       <div class="flex-1 flex overflow-hidden gap-1">
